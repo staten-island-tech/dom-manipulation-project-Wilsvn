1ec1f82f241e8d4805221d7f6db811b7e154bbc2
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-21 10:17:15 -0500

    w

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("container-box"),
-  input: document.querySelector("#input"),
+  input1: document.querySelector("#input1"),
+  input2: document.querySelector("#input2"),
+  inputs: document.querySelectorAll(".input"),
 };
-DOMSelectors.button.addEventListener("click", function(){
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input}</p>`);
-  console.log(input);
-})
+console.log(DOMSelectors.inputs);
+DOMSelectors.button.addEventListener(
+  "click",
+  function (Inject) {
+    let input1 = DOMSelectors.input1.value;
+    let input2 = DOMSelectors.input2.value;
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<p>${input1} from ${input2}</p>`
+    );
+  },
+  function (Clear) {
+    DOMSelectors.inputs.forEach((inputs) => {
+      DOMSelectors.inputs.values = null;
+    });
+  }
+);
