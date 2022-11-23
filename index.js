0aa0fe2abfd16d263438b1da82aba1f6eced49b9
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-23 09:54:47 -0500

    w

--- index.js
+++ index.js
@@ -8,23 +8,19 @@ const DOMSelectors = {
   inputs: document.querySelectorAll(".input"),
 };
 
-DOMSelectors.button.addEventListener(
-  "click",
-  function (Inject) {
-    let input1 = DOMSelectors.input1.value;
-    let input2 = DOMSelectors.input2.value;
-    let input3 = DOMSelectors.input3.value;
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `<p>${input1} from ${input2}, served in a ${input3}</p>`
-    );
-    DOMSelectors.input1.value = "";
-    DOMSelectors.input2.value = "";
-    DOMSelectors.input3.value = "";
-  })
-  DOMSelectors.button2.addEventListener(
-    "click",
-    function (clear) {
-      
-    }
-  )
+DOMSelectors.button.addEventListener("click", function Inject() {
+  let input1 = DOMSelectors.input1.value;
+  let input2 = DOMSelectors.input2.value;
+  let input3 = DOMSelectors.input3.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p>${input1} from ${input2}, served in a ${input3}</p>`
+  );
+  DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
+});
+
+DOMSelectors.button2.addEventListener("click", function clear() {
+  DOMSelectors.box.remove;
+});
