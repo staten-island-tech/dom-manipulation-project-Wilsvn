eb8deb3e3c3b79f5315528819e8e4382155dc5fe
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-22 23:25:51 -0500

    w

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
+  button2: document.getElementById("btn2"),
+  box: document.getElementById("result-box"),
   input1: document.querySelector("#input1"),
   input2: document.querySelector("#input2"),
+  input3: document.querySelector("#input3"),
   inputs: document.querySelectorAll(".input"),
 };
 
@@ -11,10 +13,18 @@ DOMSelectors.button.addEventListener(
   function (Inject) {
     let input1 = DOMSelectors.input1.value;
     let input2 = DOMSelectors.input2.value;
+    let input3 = DOMSelectors.input3.value;
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
-      `<p>${input1} from ${input2}</p>`
+      `<p>${input1} from ${input2}, served in a ${input3}</p>`
     );
-    DOMSelectors.inputs.values = "";
-})
-
+    DOMSelectors.input1.value = "";
+    DOMSelectors.input2.value = "";
+    DOMSelectors.input3.value = "";
+  })
+  DOMSelectors.button2.addEventListener(
+    "click",
+    function (clear) {
+      
+    }
+  )
