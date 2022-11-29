a7be1012c076997bd6cbb898577d6c4f8b071a76
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-29 10:17:16 -0500

    w

--- index.js
+++ index.js
@@ -8,25 +8,22 @@ const DOMSelectors = {
   inputs: document.querySelectorAll(".input"),
 };
 
-DOMSelectors.button.addEventListener(
-  "click",
-  function (Inject) {
-    let input1 = DOMSelectors.input1.value;
-    let input2 = DOMSelectors.input2.value;
-    let input3 = DOMSelectors.input3.value;
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `<p>${input1} from ${input2}, served in a ${input3}</p>`
-    );
-    DOMSelectors.input1.value = "";
-    DOMSelectors.input2.value = "";
-    DOMSelectors.input3.value = "";
-  })
-  DOMSelectors.button2.addEventListener(
-    "click",
-    function (clear) {
-      const button2 = "clear"
-      console.log(button2);
-    }
-  )
+DOMSelectors.button.addEventListener("click", function (inject) {
+  let input1 = DOMSelectors.input1.value;
+  let input2 = DOMSelectors.input2.value;
+  let input3 = DOMSelectors.input3.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p>${input1} from ${input2}, served in a ${input3}</p>`
+  );
+});
 
+DOMSelectors.button.addEventListener("click", function (empty) {
+  DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
+});
+
+DOMSelectors.button2.addEventListener("click", function (clear) {
+  Inject.target.box.remove();
+});
